ef7ec634b7da278ecee9ff080a25cd62e899eed1
Beomyong Lee <bylee@bitnine.net>
2020-02-20 21:22:57 +0900

    Add flip_database_connect.sh

---
+++ PostgreSQL/dba_scripts/flip_database_connect.sh
@@ -0,0 +1,61 @@
+#!/bin/bash
+# 특정 데이터베이스의 연결을 활성화 또는 비활성화
+
+PORT=""
+USER=""
+
+usage() {
+  echo "Usage: $0 -d <dbname> [-p <port> -U <user>]"
+  echo Database name to disable/ enable is required
+  exit 1
+}
+
+while getopts "d:p:U:u" OPT;
+do case "${OPT}" in
+      d) DBNAME=$OPTARG
+        ;;
+      p) PORT="-p $OPTARG"
+        ;;
+      U) USER="-U $OPTARG"
+        ;;
+      u) usage
+        ;;
+      [?]) usage
+  esac
+done
+
+if [ "$DBNAME" = "" ]
+  then
+    usage
+    exit 1
+fi
+
+if [ $DBNAME == "postgres" ]
+  then
+    echo Disable of postgres is prohibited
+    exit
+fi
+
+echo changin $DBNAME
+
+psql $PORT $USER -v ON_ERROR_STOP=on postgres <<_EOF_
+SELECT datname, datallowconn
+FROM pg_database
+WHERE datname = '$DBNAME' ;
+
+UPDATE pg_database
+SET datallowconn == CASE
+                    WHEN datallowconn = TRUE THEN
+                      FALSE
+                    ELSE
+                      TRUE
+                    END
+WHERE datname = '$DBNAME' ;
+
+SELECT datname, datallowconn
+FROM pg_database
+WHERE datname = '$DBNAME'
+
+\echo Connect status for $DBNAME has been reversed
+
+_EOF_

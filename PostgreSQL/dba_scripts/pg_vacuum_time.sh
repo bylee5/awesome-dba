3dcb3db0ace959df6cd057497c9c774435119008
Beomyong Lee <bylee@bitnine.net>
2020-02-28 17:08:47 +0900

    Add pg_vacuum_time.sh

---
+++ PostgreSQL/dba_scripts/pg_vacuum_time.sh
@@ -0,0 +1,63 @@
+#!/bin/bash
+# 모든 테이블 또는 선택 테이블에 대한 베큠 시간을 보인다.
+
+PORT=""
+USER=""
+TBL=""
+
+usage() {
+  echo "Usage: $0 -d <dbname> [-t <table> -p <port> -U <user>]"
+  exit 1
+}
+
+while getopts "d:p:t:U:u" OPT;
+do case "${OPT}" in
+      d) DBNAME=$OPTARG
+        ;;
+      p) PORT="-p $OPTARG"
+        ;;
+      U) USER="-U $OPTARG"
+        ;;
+      t) TBL="$OPTARG"
+        ;;
+      u) usage
+        ;;
+      [?]) usage
+  esac;
+done
+
+if [ "$DBNAME" = "" ]
+  then
+    usage
+fi
+
+getver()
+{
+VER=$(psql $PORT -t $USER $DBNAME <<_QRYVER_
+SELECT version();
+_QRYVER_
+)
+}
+
+getver
+VER=$(echo $VER | cut -c -16)
+echo "Version is $VER"
+
+vacuum_time() {
+TIMEINFO=$(psql -q $PORT $USER $DBNAME <<_QUERY_
+
+SELECT max(last_vacuum) - min(last_vacuum) as vacuum_time
+FROM pg_stat_all_tables s
+WHERE s.relname NOT LIKE 'pg_%'
+AND s.relname NOT LIKE 'sql_%'
+AND s.relname LIKE '%$TBL%'
+ORDER BY 1;
+
+_QUERY_
+)
+}
+
+vacuum_time
+echo "${TIMEINFO}"
+
+exit 0

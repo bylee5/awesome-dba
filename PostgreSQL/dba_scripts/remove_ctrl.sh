67f976b14e0a97b169ef06855dc620fc777303d7
Beomyong Lee <bylee@bitnine.net>
2020-02-29 10:42:40 +0900

    Add remove_ctrl.sh

---
+++ PostgreSQL/dba_scripts/remove_ctrl.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+OLDFILE=$1
+
+usage() {
+  echo "Usage: $0 oldfile"
+  exit 1
+}
+
+if [ -z "$OLDFILE" ]
+  then
+    usage
+    exit 1
+fi
+
+OLDFILE=$1
+NEWFILE=x$1
+
+col -bp < $OLDFILE >> $NEWFILE
+chmod +x $NEWFILE
+rm $OLDFILE
+mv $NEWFILE $OLDFILE

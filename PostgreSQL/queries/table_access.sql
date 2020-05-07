207020ebb8709966baea978bfb9be795d586297f
Beomyong Lee <bylee@bitnine.net>
2020-05-07 15:04:46 +0900

    ADD table_access.sql

---
+++ PostgreSQL/queries/table_access.sql
@@ -0,0 +1,15 @@
+--r=SELECT
+--r=SELECT
+--a=INSERT
+--w=UPDATE
+--d=DELETE
+--x=REFERENCES
+
+SELECT n.nspname AS schema,
+       c.relname,
+       array_to_string(c.relacl, '  ')
+FROM pg_class c
+JOIN pg_namespace n ON (n.oid = c.relnamespace)
+WHERE c.relkind = 'r'
+  AND n.nspname = 'public'
+ORDER BY c.relname;

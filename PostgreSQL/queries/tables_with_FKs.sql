6309399f6a6ebac36be7acbfdeb37f60bb9b5db1
Beomyong Lee <bylee@bitnine.net>
2020-05-28 21:06:59 +0900

    ADD tables_with_FKs.sql

---
+++ PostgreSQL/queries/tables_with_FKs.sql
@@ -0,0 +1,27 @@
+SELECT n.nspname AS schema,
+       t.relname AS table,
+       count(c.conname)
+FROM pg_class t
+JOIN pg_namespace n ON (n.oid = t.relnamespace)
+JOIN pg_constraint c ON (c.conrelid = t.OID AND c.contype = 'f')
+WHERE relkind = 'r'
+  AND t.relname NOT LIKE 'pg_%'
+  AND t.relname NOT LIKE 'sql_%'
+GROUP BY n.nspname,
+         t.relname
+ORDER BY count,
+         n.nspname,
+         t.relname;
+
+SELECT n.nspname AS schema,
+       t.relname AS table,
+       c.conname AS fk_name
+FROM pg_class t
+JOIN pg_namespace n ON (n.oid = t.relnamespace)
+JOIN pg_constraints c ON (c.conrelid = t.OID AND c.contype = 'f')
+WHERE relkind = 'r'
+  AND t.relname NOT LIKE 'pg_%'
+  AND t.relname NOT LIKE 'sql_%'
+ORDER BY n.nspname,
+         t.relname,
+         c.conname;

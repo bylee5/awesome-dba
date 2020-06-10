2fb341bff655d2d18b43887d0bb43d0e032ab38d
Beomyong Lee <bylee@bitnine.net>
2020-06-10 20:26:46 +0900

    Add users.sql

---
+++ PostgreSQL/queries/users.sql
@@ -0,0 +1,12 @@
+SELECT rolname AS user,
+       CASE WHEN rolcanlogin
+            THEN 'user'
+            ELSE 'group'
+        END,
+       CASE WHEN rolsuper
+            THEN 'SUPERUSER'
+            ELSE 'normal'
+        END AS super
+FROM pg_authid
+ORDER BY rolcanlogin,
+         rolname;

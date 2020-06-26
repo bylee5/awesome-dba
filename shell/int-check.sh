0e4324fc969680938dc4a5326cb9baef1b86fb31
Beomyong Lee <bylee@bitnine.net>
2020-06-26 21:17:44 +0900

    Add int-check.sh

---
+++ shell/int-check.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+
+# 14. 값이 정수인지 확인해서 계산하기
+
+# 인수가 정수인지 확인
+test "$1" -eq 0 2>/dev/null
+
+if [ $? -lt 2 ]; then
+  echo "Argument is Integer."
+  expr 10 + $1
+else
+  echo "Argument is not Integer."
+  exit 1
+fi

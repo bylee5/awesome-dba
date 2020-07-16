7677a80a49e31d631f4ea89b72dfcefa3b893507
Beomyong Lee <bylee@bitnine.net>
2020-07-16 20:38:31 +0900

    Add xargs-grep.sh

---
+++ shell/xargs-grep.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+# 32. 로그 파일이 엄청 많은 디렉터리에서
+# 파일들에 명령어를 일괄 실행하기
+
+logdir="/var/log/myapp"
+
+# 확장자 .log 파일에서 "ERROR" 문자열 검색
+find $logdir -name "*.log" -print | xargs grep "ERROR" /dev/null
